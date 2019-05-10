4fc0c6d89c6554c2f10cf4879cdd8391c427792f
Sofiane Stamboul <stamboul.sofiane@gmail.com>
2019-05-10 11:05:17 +0200

    add combinais computing function

--- src/routes/gestionnaires.js
+++ src/routes/gestionnaires.js
@@ -4,14 +4,31 @@ const { Gestionnaire } = require('../models');
 
 const router = express.Router();
 const jwtCheck = expressJwt({ secret: process.env.SECRET_KEY });
-const getAllGestionnaires = async () => Gestionnaire.find().exec();
+const getAllGestionnaires = async () => Gestionnaire.find().lean();
+const isLeaf = node => node.l === null && node.r === null;
+
+let allCombinaisons = [];
+const parcoursArbre = (combinaisons, node) => {
+  if (node === null) return;
+  if (isLeaf(node)) {
+    allCombinaisons.push(combinaisons.concat(node.x));
+  }
+  parcoursArbre(combinaisons.concat(node.x), node.l);
+  parcoursArbre(combinaisons.concat(node.x), node.r);
+};
 
 router.get('/', jwtCheck, async (req, res) => {
   const gestionnaires = await getAllGestionnaires().catch((err) => {
     res.status(500).send(err);
   });
-  console.log(gestionnaires);
-  res.send(gestionnaires);
+  const gestionnairesWithCombinaisons = gestionnaires.map((gestionnaire) => {
+    allCombinaisons = [];
+    parcoursArbre('', gestionnaire.numeros);
+    return {
+      fullname: gestionnaire.fullname,
+      combinaisons: allCombinaisons,
+    };
+  });
+  res.status(200).send(gestionnairesWithCombinaisons);
 });
-
 module.exports = router;
